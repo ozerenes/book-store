fc2b57b7f97f19a612fc49671e9b0671e141cb47
eozer <eozer@yahoo.com>
2024-01-12 21:56:14 +0300

    unused initial codes were deleted

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,10 +1,9 @@
 import { Title, Text, Anchor } from '@mantine/core';
-import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
+      <Title ta="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
           Mantine
