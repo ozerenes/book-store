b8da39bbd3c6443b749e5f7595de8fb4d401a479
eozer <eozer@yahoo.com>
2024-01-12 21:52:21 +0300

    eslint config update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,15 @@
 module.exports = {
-  extends: ['mantine'],
+  sextends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint"
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
