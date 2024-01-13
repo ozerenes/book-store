74ba80bd63cfd7254f1a1cd4c986c80eadf7b1f0
eozer <eozer@yahoo.com>
2024-01-13 23:11:46 +0300

    added colorSchemeToggle

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -5,6 +5,7 @@ import { IconBook2, IconShoppingCart } from '@tabler/icons-react';
 import { CustomNavbar } from '@/components/organisms/CustomNavbar';
 import { BrowserRouter } from 'react-router-dom';
 import { useBookVolumes } from '@/components/hooks/useData';
+import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
 export function CustomAppShell() {
   const [opened, { toggle }] = useDisclosure();
   const cart = useBookVolumes();
@@ -17,14 +18,21 @@ export function CustomAppShell() {
         padding="md"
       >
         <AppShell.Header>
-          <Group h="100%" px="md">
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <IconBook2 size={30} />
-            <Text>Book Store</Text>
-            <ActionIcon>
-              {cart.length}
-              <IconShoppingCart />
-            </ActionIcon>
+          <Group h="100%" px="md" w="100%" justify="space-between">
+            <Group>
+              <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+              <IconBook2 size={30} />
+              <Text>Book Store</Text>
+            </Group>
+            <Group>
+              <ActionIcon px={15} size="xl" miw={80} variant="default">
+                <IconShoppingCart />
+                <Text fw={500} ml={10}>
+                  {cart.length}
+                </Text>
+              </ActionIcon>
+              <ColorSchemeToggle />
+            </Group>
           </Group>
         </AppShell.Header>
         <AppShell.Navbar p="md">
