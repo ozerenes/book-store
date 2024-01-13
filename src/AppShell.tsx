0fd34b6fd357dca25bfe587871008913b1f1bec2
eozer <eozer@yahoo.com>
2024-01-13 16:18:29 +0300

    add BrowserRouter provider and cart count button

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -1,30 +1,39 @@
-import { AppShell, Burger, Group, Text } from '@mantine/core';
+import { ActionIcon, AppShell, Burger, Group, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Router } from './Router';
-import { IconBook2 } from '@tabler/icons-react';
+import { IconBook2, IconShoppingCart } from '@tabler/icons-react';
 import { CustomNavbar } from '@/components/organisms/CustomNavbar';
+import { BrowserRouter } from 'react-router-dom';
+import { useBookVolumes } from '@/components/hooks/useData';
 export function CustomAppShell() {
   const [opened, { toggle }] = useDisclosure();
+  const cart = useBookVolumes();
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <IconBook2 size={30} />
-          <Text>Book Store</Text>
-        </Group>
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
-        <CustomNavbar />
-      </AppShell.Navbar>
-      <AppShell.Main>
-        <Router />
-      </AppShell.Main>
-    </AppShell>
+    <BrowserRouter>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            <IconBook2 size={30} />
+            <Text>Book Store</Text>
+            <ActionIcon>
+              {cart.length}
+              <IconShoppingCart />
+            </ActionIcon>
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          <CustomNavbar />
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <Router />
+        </AppShell.Main>
+      </AppShell>
+    </BrowserRouter>
   );
 }
