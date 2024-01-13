3e7dc70112b43a79a1c1381f2f1f495a3508dfd0
eozer <eozer@yahoo.com>
2024-01-13 16:17:25 +0300

    add notification provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { theme } from './theme';
 import { CustomAppShell } from '@/AppShell';
+import { Notifications } from '@mantine/notifications';
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
+      <Notifications />
       <CustomAppShell />
     </MantineProvider>
   );
