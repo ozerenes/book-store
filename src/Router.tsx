dcb32765df11860336b7dc8fa1459797fee576c5
eozer <eozer@yahoo.com>
2024-01-13 23:09:01 +0300

    add home page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { BookCardGrid } from './pages/BookCardGrid';
 import { BookDetail } from '@/pages/BookDetail';
+import { HomePage } from '@/pages/HomePage';
 
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/search/:q" element={<BookCardGrid />} />
       <Route path="/details/:id" element={<BookDetail />} />
     </Routes>
