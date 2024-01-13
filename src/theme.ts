b9974fd4e5c781e97e4a7733662a8396dde2e485
eozer <eozer@yahoo.com>
2024-01-13 23:08:40 +0300

    add theme provider

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,14 @@
-import { createTheme } from '@mantine/core';
-
+import { createTheme, rem } from '@mantine/core';
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  shadows: {
+    md: '1px 1px 3px rgba(0, 0, 0, .25)',
+    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+  },
+
+  headings: {
+    fontFamily: 'Roboto, sans-serif',
+    sizes: {
+      h1: { fontSize: rem(36) },
+    },
+  },
 });
