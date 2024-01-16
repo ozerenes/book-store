d28df42b83957a1d9fed899c91807a6dc349dc9c
eozer <eozer@yahoo.com>
2024-01-17 00:29:17 +0300

    added key

--- src/components/molecules/GridSkeleton.tsx
+++ src/components/molecules/GridSkeleton.tsx
@@ -3,8 +3,8 @@ import { SimpleGrid, Skeleton } from '@mantine/core';
 export function GridSkeleton() {
   return (
     <SimpleGrid cols={{ base: 1, sm: 2, md: 2, lg: 3, xl: 4 }}>
-      {new Array(16).fill(0).map((_) => (
-        <Skeleton w={283} h={380} />
+      {new Array(16).fill(0).map((_, index) => (
+        <Skeleton key={index} w={283} h={380} />
       ))}
     </SimpleGrid>
   );
