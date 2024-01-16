3e6d5f01fb5daee5e06511c4b5df465951958af0
eozer <eozer@yahoo.com>
2024-01-16 21:40:00 +0300

    added skeleton for loading

---
+++ src/components/molecules/GridSkeleton.tsx
@@ -0,0 +1,11 @@
+import { SimpleGrid, Skeleton } from '@mantine/core';
+
+export function GridSkeleton() {
+  return (
+    <SimpleGrid cols={{ base: 1, sm: 2, md: 2, lg: 3, xl: 4 }}>
+      {new Array(16).fill(0).map((_) => (
+        <Skeleton w={283} h={380} />
+      ))}
+    </SimpleGrid>
+  );
+}
