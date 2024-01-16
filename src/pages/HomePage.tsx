13c95463ca1da314210fbec6b7d60a4a0de28a99
eozer <eozer@yahoo.com>
2024-01-16 21:39:36 +0300

    added home page for getting recommendation books

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,27 @@
-import { LibraryModel } from '@/components/organisms/LibraryModel';
+import { useEffect, useState } from 'react';
+import { BookCardGrid } from '@/pages/BookCardGrid';
+import { BookVolume } from '@/components/atoms/BookType';
+import api from '@/components/atoms/AxiosIns';
+import { GridSkeleton } from '@/components/molecules/GridSkeleton';
 
 export function HomePage() {
-  return <LibraryModel />;
+  const [data, setData] = useState<BookVolume[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  function fetchBookData() {
+    api
+      .get('/volumes', {
+        params: {
+          q: 'subject:recommendations',
+        },
+      })
+      .then((response) => setData(response?.data?.items))
+      .catch((error) => console.error('Error fetching book data:', error))
+      .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    fetchBookData();
+  }, []);
+
+  return loading ? <GridSkeleton /> : <BookCardGrid data={data} />;
 }
