806dc68e9dfba7342d7e20f20d6e30c59332bb55
eozer <eozer@yahoo.com>
2024-01-16 21:38:52 +0300

    added search page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import { BookCardGrid } from './pages/BookCardGrid';
 import { BookDetail } from '@/pages/BookDetail';
 import { HomePage } from '@/pages/HomePage';
+import { SearchPage } from '@/pages/SearchPage';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/search/:q" element={<BookCardGrid />} />
+      <Route path="/search/:q" element={<SearchPage />} />
       <Route path="/details/:id" element={<BookDetail />} />
     </Routes>
   );
---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,31 @@
+import api from '@/components/atoms/AxiosIns';
+import { useEffect, useState } from 'react';
+import { BookCardGrid } from '@/pages/BookCardGrid';
+import { useParams } from 'react-router-dom';
+import { BookVolume } from '@/components/atoms/BookType';
+import { GridSkeleton } from '@/components/molecules/GridSkeleton';
+
+export function SearchPage() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<BookVolume[]>([]);
+  const { q } = useParams();
+  function fetchBookData() {
+    api
+      .get('/volumes', {
+        params: {
+          q: q,
+        },
+      })
+      .then((response) => setData(response?.data?.items))
+      .catch((error) => console.error('Error fetching book data:', error))
+      .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    if (q) {
+      fetchBookData();
+    }
+  }, [q]);
+
+  return loading ? <GridSkeleton /> : <BookCardGrid data={data} />;
+}
