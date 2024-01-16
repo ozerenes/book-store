cd3585e7728551995dc8fac464a1bb8557114046
eozer <eozer@yahoo.com>
2024-01-16 21:40:44 +0300

    added book type

---
+++ src/components/atoms/BookType.ts
@@ -0,0 +1,81 @@
+export interface BookVolume {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    publishedDate: string;
+    description: string;
+    readingModes: {
+      text: boolean;
+      image: boolean;
+    };
+    pageCount: number;
+    printType: string;
+    categories: string[];
+    maturityRating: string;
+    allowAnonLogging: boolean;
+    contentVersion: string;
+    panelizationSummary: {
+      containsEpubBubbles: boolean;
+      containsImageBubbles: boolean;
+    };
+    imageLinks: {
+      smallThumbnail: string;
+      thumbnail: string;
+    };
+    language: string;
+    previewLink: string;
+    infoLink: string;
+    canonicalVolumeLink: string;
+  };
+  saleInfo: {
+    country: string;
+    saleability: string;
+    isEbook: boolean;
+    listPrice: {
+      amount: number;
+      currencyCode: string;
+    };
+    retailPrice: {
+      amount: number;
+      currencyCode: string;
+    };
+    buyLink: string;
+    offers: {
+      finskyOfferType: number;
+      listPrice: {
+        amountInMicros: number;
+        currencyCode: string;
+      };
+      retailPrice: {
+        amountInMicros: number;
+        currencyCode: string;
+      };
+    }[];
+  };
+  accessInfo: {
+    country: string;
+    viewability: string;
+    embeddable: boolean;
+    publicDomain: boolean;
+    textToSpeechPermission: string;
+    epub: {
+      isAvailable: boolean;
+    };
+    pdf: {
+      isAvailable: boolean;
+      acsTokenLink: string;
+    };
+    webReaderLink: string;
+    accessViewStatus: string;
+    quoteSharingAllowed: boolean;
+  };
+  searchInfo: {
+    textSnippet: string;
+  };
+  count: number;
+}
